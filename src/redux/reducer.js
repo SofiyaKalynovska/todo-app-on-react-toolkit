8dd58dbfd385cd54de73796e66de7fd9ba66f114
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-11-18 14:25:57 +0200

    Add sreateReducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,5 +1,6 @@
+import { createReducer } from "@reduxjs/toolkit";
 import { statusFilters } from "./constants";
-import { addTask, deleteTask, toggleCompleted } from "./actions";
+import { addTask, deleteTask, toggleCompleted, setStatusFilter } from "./actions";
 
 const tasksInitialState = [
   { id: 0, text: "Learn HTML and CSS", completed: true },
@@ -9,42 +10,38 @@ const tasksInitialState = [
   { id: 4, text: "Build amazing apps", completed: false },
 ];
 
-export const tasksReducer = (state = tasksInitialState, action) => {
-  switch (action.type) {
-    case addTask.type:
-      return [...state, action.payload];
-
-    case deleteTask.type:
-      return state.filter(task => task.id !== action.payload);
-
-    case toggleCompleted.type:
-      return state.map(task => {
-        if (task.id !== action.payload) {
-          return task;
-        }
-        return { ...task, completed: !task.completed };
-      });
-
-    default:
-      return state;
-  }
-};
+export const tasksReducer = createReducer(tasksInitialState, {
+  [addTask]: (state, action) => {
+    return [...state, action.payload];
+  },
+  [deleteTask]: (state, action) => {
+    return state.filter(task => task.id !== action.payload);
+  },
+  [toggleCompleted]: (state, action) => {
+    return state.map(task => {
+      if (task.id !== action.payload) {
+        return task;
+      }
+      return {
+        ...task,
+        completed: !task.completed,
+      };
+    });
+  },
+});
 
 const filtersInitialState = {
   status: statusFilters.all,
 };
 
-export const filtersReducer = (state = filtersInitialState, action) => {
-  switch (action.type) {
-    case "filters/setStatusFilter":
-      return {
-        ...state,
-        status: action.payload,
-      };
-    default:
-      return state;
-  }
-};
+export const filtersReducer = createReducer(filtersInitialState, {
+  [setStatusFilter]: (state, action) => {
+    return {
+      ...state,
+      status: action.payload,
+    };
+  },
+});
 
 
 
